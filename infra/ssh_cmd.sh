60c7a477b0df82781a19a88ec46169323db413cf
ishan <ishan1993@gmail.com>
2021-03-21 13:23:40 -0700

    use dns instead of IP

--- infra/ssh_cmd.sh
+++ infra/ssh_cmd.sh
@@ -1,4 +1,4 @@
-ssh pioneers@13.64.174.212
+ssh pioneers@project-dlh.westus.cloudapp.azure.com
 # Password will be shared on slack for safety
 # Repo is located at "/home/pioneers/workspace/dlh_project_spring2021" on the server
 # notebook is under "jupyter_root" directory within the repo
