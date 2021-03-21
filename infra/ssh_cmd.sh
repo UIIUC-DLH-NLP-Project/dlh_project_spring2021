3479bbf9cd8c35abe59209de6bd5ee859e32dda2
ishan <ishan@DESKTOP-ANNVB77.localdomain>
2021-03-21 12:52:40 -0700

    infra and jupyter docs

---
+++ infra/ssh_cmd.sh
@@ -0,0 +1,5 @@
+ssh pioneers@13.64.174.212
+# Password will be shared on slack for safety
+# Repo is located at "/home/pioneers/workspace/dlh_project_spring2021" on the server
+# notebook is under "jupyter_root" directory within the repo
+# notebook server is running in a persistent screen session, if needed (highly unlikely), run "screen -r" to see server logs
